e0dbeed7840b1526d63fbb841347dbe18c35f78b
Irina Bukharina <bukharinain@yandex.ru>
2023-06-15 04:56:47 +0300

    buttons

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -12,10 +12,9 @@ class PostRepositoryInMemoryImpl : PostRepository {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Освоение новой профессии — это не только открывающиеся возможности и перспективы, но и настоящий вызов самому себе. Приходится выходить из зоны комфорта и перестраивать привычный образ жизни: менять распорядок дня, искать время для занятий, быть готовым к возможным неудачам в начале пути. В блоге рассказали, как избежать стресса на курсах профпереподготовки → http://netolo.gy/fPD",
             published = "04 августа в 06:43",
-            likes = 5,
             likedByMe = false,
+            likes = 5,
             shares = 14,
-            shareByMe = false,
             views = 44
         ),
         Post(
@@ -23,10 +22,9 @@ class PostRepositoryInMemoryImpl : PostRepository {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Делиться впечатлениями о любимых фильмах легко, а что если рассказать так, чтобы все заскучали \uD83D\uDE34\n",
             published = "28 июля в 10:03",
-            likes = 6,
             likedByMe = false,
+            likes = 6,
             shares = 678,
-            shareByMe = false,
             views = 1000
         ),
         Post(
@@ -34,10 +32,9 @@ class PostRepositoryInMemoryImpl : PostRepository {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Таймбоксинг — отличный способ навести порядок в своём календаре и разобраться с делами, которые долго откладывали на потом. Его главный принцип — на каждое дело заранее выделяется определённый отрезок времени. В это время вы работаете только над одной задачей, не переключаясь на другие. Собрали советы, которые помогут внедрить таймбоксинг \uD83D\uDC47\uD83C\uDFFB",
             published = "22 июля в 13:01",
-            likes = 999,
             likedByMe = false,
+            likes = 999,
             shares = 555,
-            shareByMe = false,
             views = 1001
         ),
         Post(
@@ -45,10 +42,9 @@ class PostRepositoryInMemoryImpl : PostRepository {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "\uD83D\uDE80 24 сентября стартует новый поток бесплатного курса «Диджитал-старт: первый шаг к востребованной профессии» — за две недели вы попробуете себя в разных профессиях и определите, что подходит именно вам → http://netolo.gy/fQ",
             published = "05 июля в 19:10",
-            likes = 345,
             likedByMe = false,
+            likes = 345,
             shares = 10999,
-            shareByMe = false,
             views = 100000
         ),
         Post(
@@ -56,10 +52,9 @@ class PostRepositoryInMemoryImpl : PostRepository {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Диджитал давно стал частью нашей жизни: мы общаемся в социальных сетях и мессенджерах, заказываем еду, такси и оплачиваем счета через приложения.",
             published = "20 июня в 15:15",
-            likes = 1000,
             likedByMe = false,
+            likes = 1000,
             shares = 2903,
-            shareByMe = false,
             views = 3000
         ),
         Post(
@@ -67,10 +62,9 @@ class PostRepositoryInMemoryImpl : PostRepository {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Большая афиша мероприятий осени: конференции, выставки и хакатоны для жителей Москвы, Ульяновска и Новосибирска \uD83D\uDE09",
             published = "19 июня в 04:12",
-            likes = 15,
             likedByMe = false,
+            likes = 15,
             shares = 99,
-            shareByMe = false,
             views = 345
         ),
         Post(
@@ -78,10 +72,9 @@ class PostRepositoryInMemoryImpl : PostRepository {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Языков программирования много, и выбрать какой-то один бывает нелегко. Собрали подборку статей, которая поможет вам начать, если вы остановили свой выбор на JavaScript.",
             published = "01 июня в 09:44",
-            likes = 7,
             likedByMe = false,
+            likes = 7,
             shares = 45,
-            shareByMe = false,
             views = 1001
         ),
         Post(
@@ -89,10 +82,9 @@ class PostRepositoryInMemoryImpl : PostRepository {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Знаний хватит на всех: на следующей неделе разбираемся с разработкой мобильных приложений, учимся рассказывать истории и составлять PR-стратегию прямо на бесплатных занятиях \uD83D\uDC47",
             published = "29 мая в 10:12",
-            likes = 378,
             likedByMe = false,
+            likes = 378,
             shares = 1000345,
-            shareByMe = false,
             views = 10000000
         ),
         Post(
@@ -100,10 +92,9 @@ class PostRepositoryInMemoryImpl : PostRepository {
             author = "Нетология. Университет интернет-профессий будущего",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
             published = "27 мая в 11:30",
-            likes = 1023,
             likedByMe = false,
+            likes = 1023,
             shares = 2000,
-            shareByMe = false,
             views = 60000
         )
     )
@@ -119,10 +110,9 @@ class PostRepositoryInMemoryImpl : PostRepository {
                     id = nextId++,
                     author = "Me",
                     published = "now",
-                    likes = 0,
                     likedByMe = false,
+                    likes = 0,
                     shares = 0,
-                    shareByMe = false,
                     views = 0
                 )
             ) + posts
