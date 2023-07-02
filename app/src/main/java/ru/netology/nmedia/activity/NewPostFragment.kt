8f05d8b44ed10c070299ca445a2e23a4f25e72c0
Irina Bukharina <bukharinain@yandex.ru>
2023-07-02 09:34:36 +0300

    addSQLite

--- app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
@@ -1,10 +1,12 @@
 package ru.netology.nmedia.activity
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -13,16 +15,22 @@ import ru.netology.nmedia.databinding.FragmentNewPostBinding
 import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.util.StringProperty
 import ru.netology.nmedia.viewmodel.PostViewModel
+import java.io.File
 
 class NewPostFragment : Fragment() {
 
     private val viewModel: PostViewModel by viewModels(ownerProducer = ::requireParentFragment)
+    private val file = File(context?.filesDir, "Draft.txt")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentNewPostBinding.inflate(layoutInflater, container, false)
+
+        binding.addPost.setText(readText())
+        writeText("")
 
         arguments?.textArg?.let {
             binding.addPost.setText(it)
@@ -45,14 +53,40 @@ class NewPostFragment : Fragment() {
             }
             findNavController().navigateUp()
         }
+
         //для решения проблемы при редактировании поста,
         //если нет обработки нажатия кнопки назад в EditPostFragment
-        viewModel.cancelEditing()
+        //viewModel.cancelEditing()
+
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner, object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    val content = binding.addPost.text.toString()
+                    if (content.isNotBlank()) {
+                        writeText(content)
+                    }
+                    AndroidUtils.hideKeyboard(binding.addPost)
+                    findNavController().navigateUp()
+                }
+            }
+        )
 
         return binding.root
     }
 
     companion object {
         var Bundle.textArg by StringProperty
+    }
+
+    private fun writeText(content: String) {
+        context?.openFileOutput("Draft.txt", Context.MODE_PRIVATE).use {
+            it?.write(content.toByteArray())
+        }
+    }
+
+    private fun readText(): String {
+        return context?.openFileInput("Draft.txt")?.bufferedReader().use {
+            it?.readText().toString()
+        }
     }
 }
